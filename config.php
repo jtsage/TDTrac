c511fe7cf2289f1c6649843116ac021738af4f17
J.T.Sage <jtsage+tdtrac@gmail.com>
2009-02-08 23:18:43 -0500

    New Feature: Configurable day or hourly pay rates, and pay rate.
     * Add config.php options
     * Update TODO list
     * Add options to installer
     * Add lookup pay rate / day rate toggle to budget reports
     * Add lookup day rate toggle to hours displays / forms
     * Add lookup day rate toggle to email hours

--- config.php
+++ config.php
@@ -1,9 +1,11 @@
 <?php
-$TDTRAC_CPNY = "DEMO";
-$TDTRAC_SITE = "http://demo.tdtrac.com/";
 $MYSQL_SERVER = "localhost";
 $MYSQL_USER = "tdtrac";
 $MYSQL_PASS = "tdtrac";
 $MYSQL_DATABASE = "tdtrac";
 $MYSQL_PREFIX = "demo_";
+$TDTRAC_CPNY = "DEMO";
+$TDTRAC_SITE = "http://demo.tdtrac.com/";
+$TDTRAC_DAYRATE = "0";
+$TDTRAC_PAYRATE = "10";
 ?>
