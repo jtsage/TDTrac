a27568949a2a95ae83cca2ba138088ff90831c90
JTSage.com Root User <root@jtsage.com>
2010-03-25 12:37:02 -0400

    New Version: 1.2.5   Added sales tax tracking   Updated copyright for '09 and '10   Move upgrade.php -> 1.2.5   Move install.php 1.2.2 -> 1.2.5

--- install.php
+++ install.php
@@ -2,7 +2,7 @@
 ob_start(); session_start(); 
 
 ## PROGRAM DETAILS. DO NOT EDIT UNLESS YOU KNOW WHAT YOU ARE DOING
-$TDTRAC_VERSION = "1.2.2";
+$TDTRAC_VERSION = "1.2.5";
 $TDTRAC_PERMS = array("addshow", "editshow", "viewshow", "addbudget", "editbudget", "viewbudget", "addhours", "edithours", "viewhours", "adduser");
 $INSTALL_FILES = array(
 	"index.php",
