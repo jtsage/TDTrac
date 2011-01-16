454b0af260eb01deab1a2db8cde1958845bc4003
root <root@jtsage.com>
2011-01-16 17:57:37 -0500

    Remove bad config again, oops.

--- config.php
+++ config.php
@@ -16,30 +16,3 @@ $TDTRAC_SITE = "http://demo.tdtrac.com/";
 $TDTRAC_DAYRATE = "0";
 $TDTRAC_PAYRATE = "10";
 ?>
-<?php
-$TDTRAC_CPNY = "TODD";
-$TDTRAC_SITE = "http://todd.tdtrac.com/";
-$TDTRAC_DAYRATE = "0";
-$TDTRAC_PAYRATE = "10";
-?>
-<?php
-$MYSQL_SERVER = "mysql.jtsage.com";
-$MYSQL_USER = "tdtrac";
-$MYSQL_PASS = "";
-$MYSQL_DATABASE = "tdtrac";
-$MYSQL_PREFIX = "todd_";
-?>
-<?php
-$MYSQL_SERVER = "mysql.jtsage.com";
-$MYSQL_USER = "tdtrac";
-$MYSQL_PASS = "";
-$MYSQL_DATABASE = "tdtrac";
-$MYSQL_PREFIX = "todd_";
-?>
-<?php
-$MYSQL_SERVER = "mysql.jtsage.com";
-$MYSQL_USER = "tdtrac";
-$MYSQL_PASS = "tdtrac";
-$MYSQL_DATABASE = "tdtrac";
-$MYSQL_PREFIX = "todd_";
-?>
