2360de33a1bd9607343278108c982eaf8405d93a
J.T.Sage <jtsage+tdtrac@gmail.com>
2011-03-04 19:32:51 -0500

    Move test mode switch to config.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ ob_start(); session_start();
 ## PROGRAM DETAILS. DO NOT EDIT UNLESS YOU KNOW WHAT YOU ARE DOING
 $TDTRAC_VERSION = "2.0.1";
 $TDTRAC_DBVER = "2.0.1";
-$TEST_MODE = true;
+$TEST_MODE = false;
 $SITE_SCRIPT = array('');
 $CANCEL = false;
 $HEAD_LINK = array('');
